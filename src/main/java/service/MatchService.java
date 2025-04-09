0a14796d6595b3d8ef4fff302ca55e3d13193a44
clayne <savva665544332211@yandex.ru>
2025-04-10 01:34:41 +0300

    create: MatchService

---
+++ src/main/java/service/MatchService.java
@@ -0,0 +1,50 @@
+package service;
+
+import dao.MatchRepository;
+import dto.MatchCreateDto;
+import dto.MatchReadDto;
+import dto.PlayerReadDto;
+import entity.Match;
+import entity.Player;
+import lombok.RequiredArgsConstructor;
+import mapper.MatchCreateMapper;
+import mapper.MatchReadMapper;
+import mapper.PlayerReadMapper;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class MatchService {
+    private final MatchRepository matchRepository;
+    private final MatchCreateMapper matchCreateMapper;
+    private final MatchReadMapper matchReadMapper;
+    private final PlayerReadMapper playerReadMapper;
+
+    public Long create(MatchCreateDto matchDto) {
+        Match match = matchCreateMapper.mapFrom(matchDto);
+        return matchRepository.save(match).getId();
+    }
+
+    public boolean delete(Long id) {
+        var maybeMatch = matchRepository.findById(id);
+        maybeMatch.ifPresent(match -> matchRepository.delete(id));
+        return maybeMatch.isPresent();
+    }
+
+    public Optional<MatchReadDto> findMatchById(Long id) {
+        return matchRepository.findById(id).map(matchReadMapper::mapFrom);
+    }
+
+    public List<MatchReadDto> findAllMatches() {
+        return matchRepository.findAll().stream().map(matchReadMapper::mapFrom).toList();
+    }
+
+    public List<MatchReadDto> findMatchByPlayer(PlayerReadDto playerReadDto) {
+        return matchRepository.findByPlayer(Player.builder()
+                                .id(playerReadDto.id())
+                                .name(playerReadDto.name())
+                                .build())
+                .stream().map(matchReadMapper::mapFrom).toList();
+    }
+}
